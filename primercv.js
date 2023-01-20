10373a7a85e29e9da6446922dafb9cf984e2af3c
MaAylenB <102624861+MaAylenB@users.noreply.github.com>
2023-01-20 17:05:47 -0300

    Add files via upload

---
+++ primercv.js
@@ -0,0 +1,12 @@
+document.getElementById('botoncolor') .addEventListener('click', function () {
+    document.body.style.backgroundColor = '#ddf2c9'
+})
+
+document.getElementById('botoneliminar') .addEventListener('click', function () {
+    document.getElementById('demo').style.display = 'none'
+})
+
+
+
+
+
